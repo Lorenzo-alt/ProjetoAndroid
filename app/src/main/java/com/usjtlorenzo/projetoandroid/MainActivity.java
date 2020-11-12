1e515b92b7ca7d22cf6cc8fa5c15cd616e854195
Lorenzo <lorenzo.juliati@terra.com.br>
2020-11-11 21:04:11 -0300

    Atualizando o github

--- app/src/main/java/com/usjtlorenzo/projetoandroid/MainActivity.java
+++ app/src/main/java/com/usjtlorenzo/projetoandroid/MainActivity.java
@@ -6,17 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button bt_login;
+    private Button bt_login;
+    private TextView tv_registrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setBackgroundDrawableResource(R.drawable.backgroundnovo);
 
-        bt_login = findViewById(R.id.bt_login);
+        bt_login = findViewById(R.id.bt_registrar);
+        tv_registrar = findViewById(R.id.tv_registrar);
 
         bt_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
                         "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla dapibus lectus lacus, sit amet facilisis augue porta vel. Nunc eleifend sit amet libero nec feugiat. Sed scelerisque ante id maximus scelerisque. Pellentesque porta sem ut felis porta finibus. Proin quis risus porttitor, viverra nibh sed, convallis mi. Proin molestie dictum odio, quis porta diam. In mi orci, consequat id gravida sit amet, volutpat ac neque. Praesent a lorem at neque accumsan consectetur. Ut in tortor lacus. In lobortis augue ac posuere varius. Duis vehicula ut lorem ac sagittis. Fusce eu quam venenatis odio luctus iaculis. Sed interdum maximus leo, a maximus leo suscipit quis. Donec nunc nunc, feugiat eget suscipit suscipit, sollicitudin eu lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;\n" +
                         "\n" +
                         "Maecenas diam ante, tempus vel elit in, pharetra scelerisque leo. Nullam sit amet quam at sapien semper vulputate. Pellentesque mattis nulla id quam elementum rutrum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse nulla est, egestas eget erat et, commodo molestie libero. Etiam sollicitudin libero vel velit blandit, id egestas nisi finibus. Aliquam tristique nibh at dolor mollis, at maximus ante fermentum. Donec et nunc in lectus tincidunt ullamcorper. Pellentesque et efficitur ante, sed accumsan odio. Praesent pulvinar dapibus sem ut scelerisque. Integer suscipit ipsum eu elit efficitur aliquet. Nam feugiat ultricies purus, vel egestas tellus eleifend eu.");
+                startActivity(i);
+            }
+        });
+        tv_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), TelaRegistrar.class);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/usjtlorenzo/projetoandroid/TelaPrincipal.java
+++ app/src/main/java/com/usjtlorenzo/projetoandroid/TelaPrincipal.java
@@ -1,21 +1,61 @@
 package com.usjtlorenzo.projetoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+
+import com.google.android.material.navigation.NavigationView;
+
+import static com.usjtlorenzo.projetoandroid.R.drawable.backgroundnovo;
 
 public class TelaPrincipal extends AppCompatActivity {
 
-    EditText et_texto;
+    private RecyclerView rv_lembretes;
+
+    private DrawerLayout dl;
+    private NavigationView navigationView;
+    private String l1[], l2[], l3[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+        getWindow().setBackgroundDrawableResource(R.drawable.telaprincipal);
 
-        et_texto = findViewById(R.id.et_texto);
-        et_texto.setText(getIntent().getStringExtra("texto"));
+        dl = findViewById(R.id.drawerLayout);
+        findViewById(R.id.logotb).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dl.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView = findViewById(R.id.nav_principal);
+        navigationView.setItemIconTintList(null);
+
+        rv_lembretes = findViewById(R.id.rv_lembretes);
+
+        l1 = getResources().getStringArray(R.array.tarefas);
+        l2 = getResources().getStringArray(R.array.datas_cadastro);
+        l3 = getResources().getStringArray(R.array.datas_previsao);
+
+        MeuAdapter meuAdapter = new MeuAdapter(this, l1, l2, l3);
+        rv_lembretes.setAdapter(meuAdapter);
+        rv_lembretes.setLayoutManager( new LinearLayoutManager(this));
+        rv_lembretes.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL){
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+                super.onDraw(c, parent, state);
+            }
+        });
     }
 }
